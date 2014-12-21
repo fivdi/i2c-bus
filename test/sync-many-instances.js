c4211b8a17a04e05b7bad76124d86d2f1f26fd3e
fivdi <fivdiweb@gmail.com>
2014-12-22 00:52:39 +0100

    test two bus objects for same bus concurrently

--- test/sync-many-instances.js
+++ test/sync-many-instances.js
@@ -3,7 +3,12 @@
 var i2c = require('../');
 
 var DS1621_ADDR = 0x48,
-  CMD_ACCESS_TL = 0xa2;
+  DS1621_CMD_ACCESS_TH = 0xa1,
+  DS1621_CMD_ACCESS_TL = 0xa2;
+
+var TSL2561_ADDR = 0x39,
+  TSL2561_CMD = 0x80,
+  TSL2561_REG_ID = 0x0a;
 
 function useBusMoreThanMaxFdTimes() {
   var i2c1,
@@ -14,7 +19,7 @@ function useBusMoreThanMaxFdTimes() {
   // that file descriptors are being freed.
   for (i = 1; i <= 2000; i += 1) {
     i2c1 = i2c.openSync(1);
-    i2c1.readWordSync(DS1621_ADDR, CMD_ACCESS_TL);
+    i2c1.readWordSync(DS1621_ADDR, DS1621_CMD_ACCESS_TL);
     i2c1.closeSync();
   }
 }
@@ -29,16 +34,27 @@ function useMultipleObjectsForSameBusConcurrently() {
     buses.push(i2c.openSync(1));
   }
   buses.forEach(function (bus) {
-    bus.readWordSync(DS1621_ADDR, CMD_ACCESS_TL);
+    bus.readWordSync(DS1621_ADDR, DS1621_CMD_ACCESS_TL);
   });
   buses.forEach(function (bus) {
     bus.closeSync();
   });
 }
 
+function useTwoObjectsForSameBusConcurrently() {
+  var ds1621 = i2c.openSync(1),
+    tsl2561 = i2c.openSync(1),
+    ds1621TempHigh = ds1621.readWordSync(DS1621_ADDR, DS1621_CMD_ACCESS_TH),
+    tsl2561Id = tsl2561.readByteSync(TSL2561_ADDR, TSL2561_CMD | TSL2561_REG_ID);
+
+  console.log("  ds1621TempHigh: " + ds1621TempHigh);
+  console.log("  tsl2561Id: " + tsl2561Id);
+}
+
 (function () {
   useBusMoreThanMaxFdTimes();
   useMultipleObjectsForSameBusConcurrently();
+  useTwoObjectsForSameBusConcurrently();
   console.log('ok - sync-many-instances');
 }());
 
