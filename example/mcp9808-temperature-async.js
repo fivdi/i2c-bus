52650f5616f68f51129da3bae163ebc54763e91e
fivdi <bdscooke@gmail.com>
2019-09-15 19:48:17 +0200

    fix lint errors

--- example/mcp9808-temperature-async.js
+++ example/mcp9808-temperature-async.js
@@ -15,15 +15,21 @@ const toCelsius = (rawData) => {
 };
 
 const i2c1 = i2c.open(1, (err) => {
-  if (err) throw err;
+  if (err) {
+    throw err;
+  }
 
   i2c1.readWord(MCP9808_ADDR, TEMP_REG, (err, rawData) => {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    }
 
     console.log(toCelsius(rawData));
 
     i2c1.close((err) => {
-      if (err) throw err;
+      if (err) {
+        throw err;
+      }
     });
   });
 });
