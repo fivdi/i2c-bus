64134625bc876708d2db8eb87174ee3a2b5ac54d
fivdi <bdscooke@gmail.com>
2021-09-26 20:07:35 +0200

    fix lint issues in i2c-list-busses.js and add glob as a development dependency

--- example/i2c-list-busses.js
+++ example/i2c-list-busses.js
@@ -1,11 +1,16 @@
-//i2c-bus doesn't have an API for this purpose. However, glob and regular expressions could be used to retrieve a list of I2C bus numbers. Something like this should do it:
+'use strict';
+
+// Determine the bus numbers of the I2C busses available on the current
+// machine and print those bus numbers to the screen.
 
 const glob = require('glob');
 
 const busNumbers = glob.sync('/dev/i2c-*').
-  filter(fileName => fileName.match(/\/i2c-\d+$/) != null).
+  filter(fileName => fileName.match(/\/i2c-\d+$/) !== null).
   map(fileName => parseInt(fileName.match(/\d+$/)[0], 10));
 
 console.log(busNumbers);
 
-// Here glob is used synchronously but it can also be used asynchronously. busNumbers is an array of numbers.
+// Here glob is used synchronously but it can also be used asynchronously.
+// busNumbers is an array of numbers.
+
